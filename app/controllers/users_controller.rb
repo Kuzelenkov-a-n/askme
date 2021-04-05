d07ac12825fa659d006e9ed2cf07659004b72862
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-04-05 18:35:21 +0300

    assets, views, routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
 
   def show
-    @hello = 'Привет, Андрей!'
+    @user = User.new(
+      name: 'Andrew',
+      username: '_andrew_',
+      avatar_url: 'https://goodprogrammer.ru/system/avatars/000/016/211/aa680b6a02af38e87e25667db84ccbd62375de40_x300.jpg?1607677928'
+    )
   end
 end
