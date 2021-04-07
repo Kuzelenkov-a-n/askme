7a7b89713f425cf82d77e382ec42c6022d0d9f15
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-04-07 14:03:22 +0300

    Delete partial, modified question_helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,10 +1,10 @@
 module QuestionsHelper
   def correct_of_declination(question_count)
-    ostatok100 = (question_count % 100).to_s
-    case ostatok100
-    when /^.*[5-90]$|^.*11$/ then "#{question_count} вопросов"
-    when /^.*[2-4]$/ then "#{question_count} вопроса"
-    else "#{question_count} вопрос"
-    end
+    ostatok10 = question_count % 10
+    ostatok100 = question_count % 100
+    return "#{question_count} вопросов" if ostatok100.between?(11, 14)
+    return "#{question_count} вопрос" if ostatok10 == 1
+    return "#{question_count} вопроса" if ostatok10.between?(2, 4)
+    return "#{question_count} вопросов" if ostatok10.between?(5, 9) || ostatok10.zero?
   end
 end
