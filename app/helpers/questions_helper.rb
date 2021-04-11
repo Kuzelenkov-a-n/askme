ce76b14d99036136b3b360a6a4d8f777afe15eaf
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-04-11 18:54:17 +0300

    question_helper corrected

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,9 +2,9 @@ module QuestionsHelper
   def correct_of_declination(count, vopros, voprosa, voprosov)
     ostatok10 = count % 10
     ostatok100 = count % 100
-    voprosov if ostatok100.between?(11, 14)
-    vopros if ostatok10 == 1
-    voprosa if ostatok10.between?(2, 4)
-    voprosov
+    return voprosov if ostatok100.between?(11, 14)
+    return vopros if ostatok10 == 1
+    return voprosa if ostatok10.between?(2, 4)
+    return voprosov
   end
 end
