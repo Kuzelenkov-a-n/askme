b2a338ccdf2cbaf301f82306ffb4505e9b9b61c2
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-04-11 12:07:35 +0300

    Color to user, question_helper, views

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,10 +1,10 @@
 module QuestionsHelper
-  def correct_of_declination(question_count)
-    ostatok10 = question_count % 10
-    ostatok100 = question_count % 100
-    return "#{question_count} вопросов" if ostatok100.between?(11, 14)
-    return "#{question_count} вопрос" if ostatok10 == 1
-    return "#{question_count} вопроса" if ostatok10.between?(2, 4)
-    return "#{question_count} вопросов" if ostatok10.between?(5, 9) || ostatok10.zero?
+  def correct_of_declination(count, vopros, voprosa, voprosov)
+    ostatok10 = count % 10
+    ostatok100 = count % 100
+    voprosov if ostatok100.between?(11, 14)
+    vopros if ostatok10 == 1
+    voprosa if ostatok10.between?(2, 4)
+    voprosov
   end
 end
