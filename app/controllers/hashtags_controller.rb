8fc61298bf31fb33145c1f11cff79c8168b31330
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-05-02 15:52:16 +0300

    404

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,5 +1,7 @@
 class HashtagsController < ApplicationController
   def show
     @hashtag = Hashtag.find_by(text: params[:text])
+
+    render file: "#{Rails.root}/public/404.html", layout: false unless @hashtag.present?
   end
 end
